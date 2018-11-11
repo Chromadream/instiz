e9562380976f9b4bf2a9acba20abcf548120b09a
Jonathan Nicholas <jonicrecis@gmail.com>
2018-11-12 02:24:42 +1100

    some testing addition

--- tests/test_ichart.py
+++ tests/test_ichart.py
@@ -16,3 +16,7 @@ def test_refresh(class_init):
     x = class_init.refresh()
     assert isinstance(x, iChart)
 
+
+def test_get_rank(class_init):
+    x = class_init.get_next_entry()
+    assert next(x).rank == 1
